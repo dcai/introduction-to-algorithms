38a5bfa68e849afcb464a26bccd1c96f533b1339
Dongsheng Cai <imdongsheng@gmail.com>
2012-03-01 23:23:49 +0800

    Heapsort implementation

---
+++ 6-heapsort/6-1_heapsort.c
@@ -0,0 +1,105 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+#define LIST_LEN 10
+
+void print_list(int list[], int length) {
+    printf("length: %d\n", length);
+    printf("List: [");
+    int counter = 0;
+    for (counter; counter < length; counter++) {
+        printf("%d, ", list[counter]);
+    }
+    printf("] \n");
+}
+
+int parent(int i) {
+    /*return (int)floor(i/2);*/
+    // divide is right shift
+    return i >> 1;
+}
+
+int left(int i) {
+    // 2 * i
+    return (i << 1) + 1;
+}
+
+int right(int i) {
+    // 2 * i + 1
+    return (i << 1) + 2;
+}
+
+void max_heapify(int list[], int i, int heap_size) {
+    int l = left(i);
+    int r = right(i);
+    int largest;
+    int temp;
+
+    if ((l < heap_size) && (list[l] > list[i])) {
+        largest = l;
+    } else {
+        largest = i;
+    }
+
+    if ((r < heap_size) && (list[r] > list[largest])) {
+        largest = r;
+    }
+
+    if (largest != i) {
+        temp = list[i];
+        list[i] = list[largest];
+        list[largest] = temp;
+        max_heapify(list, largest, heap_size);
+    }
+    
+}
+
+void build_max_heap(int list[], int length) {
+    int i;
+    for (i = (length-1)/2; i >=0; i--) {
+        max_heapify(list, i, length);
+    }
+}
+
+void heap_sort(int list[], int length) {
+    build_max_heap(list, length);
+
+    int temp;
+    int i;
+
+    for (i = length - 1; i > 0; i--) {
+        temp = list[0];
+        list[0] = list[i];
+        list[i] = temp;
+        max_heapify(list, 0, i);
+    }
+}
+
+int main(int argc, char *argv[]) {
+    unsigned int seed = (unsigned int)time(NULL);
+    srand(seed);
+
+    int list[LIST_LEN];
+    int counter = 0;
+    int number = 0;
+
+    // test function pointer
+    void (*sort)(int [], int) = heap_sort;
+
+    for (counter; counter < LIST_LEN; counter++) {
+        number = rand() % 1000;
+        // use rand() % 1000 will get random number between 1-1000
+        list[counter] = number;
+    }
+    printf("parent of %d is %d\n", 7, parent(7));
+    printf("left of %d is %d\n", 7, left(7));
+    printf("right of %d is %d\n", 7, right(7));
+    print_list(list, LIST_LEN);
+
+    sort(list, LIST_LEN);
+
+    print_list(list, LIST_LEN);
+
+    return 0;
+}
